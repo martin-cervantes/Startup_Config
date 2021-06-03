4e9980102053476aa75f725698f39f2597315373
Martin Cervantes <cervantes.martine@gmail.com>
2021-06-03 13:43:02 -0600

    Add pgadmin to ruby installation

--- 04_install_ruby.sh
+++ 04_install_ruby.sh
@@ -4,11 +4,11 @@ echo "===> Installing Yarn"
 
 apt install gcc g++ make curl -y
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-apt-get install yarn -y
+apt install yarn -y
 
 yarn --version
 
@@ -22,7 +22,18 @@ apt install nodejs -y
 
 echo "===> Installing rbenv"
 
-apt install rbenv -y
+apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+source ~/.bashrc
+
+type rbenv
+
 
 echo "===> Installing Ruby"
 
@@ -73,6 +84,48 @@ echo "sudo -u postgres createuser --interactive"
 
 read response
 
+echo
+echo "* * * * * * * * * * * * * * * * * * * * * * *"
+echo
+echo "Modify password for role postgres:"
+echo "sudo -u postgres psql postgres"
+echo 
+echo "alter user postgres with password 'postgres';"
+
+read response
+
+#
+# Setup the repository
+#
+
+# Install the public key for the repository (if not done previously):
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#
+# Install pgAdmin
+#
+
+# Install for both desktop and web modes:
+sudo apt install pgadmin4
+
+# Install for web mode only: 
+sudo apt install pgadmin4-web 
+
+# Configure the webserver, if you installed pgadmin4-web:
+sudo /usr/pgadmin4/bin/setup-web.sh
+
+echo
+echo "* * * * * * * * * * * * * * * * * * * * * * *"
+echo
+echo "Using pgadmin create a connection to localhost"
+echo
+echo "     name:  localhost"
+echo "host name:  localhost"
+echo " username:  postgres"
+echo " password:  postgres"
 echo
 echo "* * * * * * * * * * * * * * * * *"
 echo "* * *   Finish! All done!   * * *"
