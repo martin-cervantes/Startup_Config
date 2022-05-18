60ccfe20e9ae441a81b737ea6a243083e6f47b5a
Martin Cervantes <cervantes.martine@gmail.com>
2022-05-18 09:01:54 -0600

    Add update to rbenv installation

--- 04_install_ruby.sh
+++ 04_install_ruby.sh
@@ -23,7 +23,7 @@ yarn --version
 
 echo "===> Installing rbenv"
 
-sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev -y
+sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev -y
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
